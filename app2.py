d864fe9a3c41bf158ce7f64b6ad8d6d98eb7b52c
audrilli <audreymeret.lohmann@zhdk.ch>
2023-12-01 23:58:19 +0100

    Add app2 reading pixels

---
+++ app2.py
@@ -0,0 +1,25 @@
+# Third party modules
+import numpy
+from PIL import Image
+
+
+def get_image(image_path):
+    """Get a numpy array of an image so that one can access values[x][y]."""
+    image = Image.open(image_path, "r")
+    width, height = image.size
+    pixel_values = list(image.getdata())
+    if image.mode == "RGB":
+        channels = 3
+    elif image.mode == "L":
+        channels = 1
+    else:
+        print("Unknown mode: %s" % image.mode)
+        return None
+    pixel_values = numpy.array(pixel_values).reshape((width, height, channels))
+    return pixel_values
+
+
+image = get_image("image.png")
+
+print(image[0])
+print(image.shape)
