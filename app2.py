33e6d72cbf7dbd1ccfbc7ce74deb8188877f1971
audrilli <audreymeret.lohmann@zhdk.ch>
2023-12-03 23:40:50 +0100

    Delete comments

--- app2.py
+++ app2.py
@@ -1,35 +1,4 @@
-# # Third party modules
-# import numpy
-# from PIL import Image
 
-
-# def get_image(image_path):
-#     """Get a numpy array of an image so that one can access values[x][y]."""
-#     image = Image.open(image_path, "r")
-#     width, height = image.size
-#     pixel_values = list(image.getdata())
-#     if image.mode == "RGB":
-#         channels = 3
-#     elif image.mode == "L":
-#         channels = 1
-#     else:
-#         print("Unknown mode: %s" % image.mode)
-#         return None
-#     pixel_values = numpy.array(pixel_values).reshape((width, height, channels))
-#     return pixel_values
-
-
-# image = get_image("1080x1080-image2.png")
-
-# print(image[0])
-# print(image.shape)
-
-#Code from https://stackoverflow.com/questions/138250/how-to-read-the-rgb-value-of-a-given-pixel-in-python
-
-
-#Pixel Sorting algo
-# Third party modules
-# Third party modules
 import numpy as np
 from PIL import Image
 
